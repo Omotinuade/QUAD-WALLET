e534d006013cbf91108bc5c54dd2cae6cfef71f3
OMOTINUADE <speaktoyin5@gmail.com>
2023-04-20 12:47:57 +0100

    new update

--- test/test_UserRepository.py
+++ test/test_UserRepository.py
@@ -1,18 +1,22 @@
 from unittest import TestCase
-from models import User
-from repositories import UserRepositoryImpl
+
+from data.models.User import User
+from data.repositories.UserRepositoryImpl import UserRepositoryImpl
 
 
 class TestUserRepository(TestCase):
 
     def setUp(self) -> None:
-        self.repository = UserRepositoryImpl
-
-    user = User()
-    user.set_user_name("John")
-
+        self.repository = UserRepositoryImpl()
+        self.user = User()
+        self.user.set_email_address("benten")
+        self.user.set_password("2121")
+        self.user.set_phone_number("08138732503")
+        self.user.set_account_number(self.user.get_phone_number()[1:])
 
     def test_save_user(self):
-        self.fail()
+        result = self.repository.save_user(self.user)
+        self.assertEquals(1, result.get_user_id())
+        self.assertEquals(result.get_email_address(), self.user.get_email_address())
 
     # def test_find
